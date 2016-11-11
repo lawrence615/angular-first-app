05322599004ce553e7dfb5a8b2dbee9c261fd4ad
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-11-11 17:11:57 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>XS Millionaires 2016</h1>
+    <p>Ladies Night!!!</p>
+    <p>Baricho Road</p>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works!!!!';
 }
